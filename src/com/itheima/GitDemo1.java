27dde02613027eec7d22f520bf50390add1251ad
xusiyu <xusiyu@qq.com>
2019-03-21 21:41:48 +0800

    添加大本地

---
+++ src/com/itheima/GitDemo1.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class GitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("德玛西亚万岁....");
+        System.out.println("德玛西亚万岁....");
+        System.out.println("德玛西亚万岁....");
+        System.out.println("德玛西亚万岁....");
+    }
+}
